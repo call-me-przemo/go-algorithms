626f6a7c597716b4c5b880e5f886d2145d49c5bf
Przemysław Mielniczek <pmiel13@gmail.com>
2024-06-11 20:24:02 +0200

    prime number check

---
+++ src/is-prime-number.go
@@ -0,0 +1,17 @@
+package src
+
+import "math"
+
+func IsPrimeNumber(number int) bool {
+	if number < 2 {
+		return false
+	}
+
+	for i := 2; i <= int(math.Sqrt(float64(number))); i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
